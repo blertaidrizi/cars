9d49502d9d4fa6d2b11f6f650013552bedc91d94
Blerta <blertaaidrizii@gmail.com>
2025-01-05 19:52:11 +0100

    Second Version

--- CarGo/Program.cs
+++ CarGo/Program.cs
@@ -1,14 +1,18 @@
 using CarGo.Components;
 using CarGo.Data;
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddBootstrapBlazor();
+
 builder.Services.AddDbContext<appDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
